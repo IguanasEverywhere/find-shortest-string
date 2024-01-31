64f7fa899aa032bfd9e9b6a145d461f660d2a55f
Scott Schwab <scottschwab86@gmail.com>
2024-01-31 10:55:29 -0600

    Complete challenge

--- javascript/find_shortest_string.js
+++ javascript/find_shortest_string.js
@@ -1,5 +1,28 @@
 function findShortestString(arr) {
   // type your code here
+
+  // have array for string lengths = []
+  // iterate over arr
+    // for each element, push length of element to lengths array
+    // sort the lengths array
+  // iterate over arr again
+    // return first element that has length of first element from lengths
+
+  let lengths = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    lengths.push(arr[i].length);
+  }
+
+  let sortedLengths = lengths.sort();
+
+  for (let j = 0; j < arr.length; j++) {
+    if (arr[j].length === sortedLengths[0]) {
+      return arr[j];
+    }
+  }
+
+
 }
 
 if (require.main === module) {
@@ -17,7 +40,33 @@ if (require.main === module) {
   console.log("Expecting: 'lily'");
   console.log("=>", findShortestString(['flower', 'juniper', 'lily', 'dadelion']));
 
+  console.log("");
+
+  console.log("Expecting: ''");
+  console.log("=>", findShortestString(['flower', "", 'juniper', 'lily', 'dadelion']));
+
+  console.log("");
+
+  console.log("Expecting: 'a'");
+  console.log("=>", findShortestString(['a']));
+
   // BENCHMARK HERE
+  let longArr = [];
+  for (let j = 0; j < 100; j++) {
+    longArr.push("a" * Math.floor(Math.random() * 30))
+  }
+
+  let startTime = Date.now();
+  for (let i = 0; i < 1000; i++) {
+    findShortestString("aaa", "aa", "a");
+    findShortestString(longArr);
+  }
+  let endTime = Date.now()
+
+  let averageTime = ((endTime-startTime) / 2000);
+  console.log(averageTime)
+
+
 }
 
 module.exports = findShortestString;
